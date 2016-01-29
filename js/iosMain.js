7ef667c4a4cdf4071df0f24cbc3cbe492922864d
wallowbear <culapicar@163.com>
2016-01-29 13:50:57 +0800

    修正坐标问题

--- js/iosMain.js
+++ js/iosMain.js
@@ -9,29 +9,17 @@ $(function(){
     });
 	
 	var UL=$(".iosContainer");
+	UL.css("margin-top",500);
 	
-	UL.mouseenter(function(e) {
-        console.log("enter");
-		
-		/*	
-		UL.mouseleave(function(e3) {
-           //UL.mouseenter();
-		   console.log("leave")
-		   UL.mousemove();
-		   UL.mouseleave();
-			
-        });		*/	
-		
-    });
-	
+	//alert();
 	//UL.children().mousemove(function(e){e.preventDefault();});
 	UL.mousemove(function(e2){	  
-		if(e2.currentTarget!=e2.target)return;
+		
 		var UL2=UL.find("ul");
 		
-		UL2.css("top",-e2.offsetY/600*(UL2.height()-600));
+		UL2.css("top",-(e2.clientY-parseInt(UL.css("margin-top")))/600*(UL2.height()-600));
 		console.log("enter move",e2.offsetY);	
 	})
 	
-	UL.on("mouseleave",function(){console.log("mouseleave")});
+	//UL.on("mouseleave",function(){console.log("mouseleave")});
 })
